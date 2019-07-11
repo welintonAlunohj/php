8030d6ae6b9d9df4b545a3d04080ea9cfab16515
welintonAlunohj <52757012+welintonAlunohj@users.noreply.github.com>
2019-07-11 19:31:40 -0300

    correção de momento

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 
  echo"Olá mundo, eu sou Welinto! o grande";
- echo "O cara que gosta da Amanda Raquely e ela gosta de tbm";
+ echo "O cara que gosta da Amanda Raquely e ela gosta mim";
+ echo "Mas ela não admite";
 
 
 ?>

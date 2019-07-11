56ee7af58050a2460eed24157e17b4e05e44353d
welinton <wel.imper@gmail.com>
2019-07-11 19:26:13 -0300

    resolvi agora o conflito

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
  echo"Olá mundo, eu sou Welinto! o grande";
  echo "O cara que gosta da Amanda Raquely";
-=======
- echo"Olá mundo, eu sou Welinto!";
- echo "A Amanda Raquely gosta de mim";
->>>>>>> 6397665ea88ae18a055e1a9a8a51a1c08b8b9084
+
 
 ?>

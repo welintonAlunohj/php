f70e11a84a86c53237c73c09f79e3626bdab54ff
welintonAlunohj <52757012+welintonAlunohj@users.noreply.github.com>
2019-07-11 15:08:26 -0300

    mudei o texto

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-<<<<<<< HEAD
  echo"Olá mundo, eu sou Welinto muito bem!";
- echo "fizeram certo";
-=======
- echo"Olá mundo, eu sou Welinton estão fazendo errado!";
- echo "podem se organizar";
->>>>>>> 391d8794b9d1a76255ad3cb2a09b06875c3eb952
+ echo "podem melhor";
 
 ?>
